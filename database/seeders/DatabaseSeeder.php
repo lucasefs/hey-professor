69e00185bf04faa012ec1d287c07ebb828a7d429
Lucas Eduardo Fagundes <lucasfagundes0@outlook.com>
2024-02-23 20:47:13 -0300

    HEY-9: auto login when the env is Local

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Question;
+use App\Models\{Question, User};
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->count(1)->create();
         Question::factory()->count(10)->create();
     }
 }
