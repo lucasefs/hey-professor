9fe2b74fd6ce2112028dd2bc28c4fc945beff26d
Lucas Eduardo Fagundes <lucasfagundes0@outlook.com>
2024-03-08 15:58:46 -0300

    hey-14: refactoring CreateQuestionTest

--- tests/Feature/Question/CreateTest.php
+++ tests/Feature/Question/CreateTest.php
@@ -4,67 +4,67 @@ use App\Models\User;
 
 use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, post};
 
-beforeEach(function () {
-    //Arrange :: preparar
-    $user = User::factory()->create();
-    actingAs($user);
-});
+describe('authenticated user', function () {
 
-it('should be able to create a new question bigger than 255 characters', closure: function () {
+    beforeEach(function () {
+        //Arrange :: preparar
+        $user = User::factory()->create();
+        actingAs($user);
+    });
 
-    //Act :: agir
-    $request = post(route('question.store'), [
-        'question' => str_repeat('*', 260) . '?',
-    ]);
+    it('should be able to create a new question bigger than 255 characters', closure: function () {
 
-    //Assert :: verificar
-    $request->assertRedirect(route('dashboard'));
-    assertDatabaseCount('questions', 1);
-    assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?',
-    ]);
-});
+        //Act :: agir
+        $request = post(route('question.store'), [
+            'question' => str_repeat('*', 260) . '?',
+        ]);
 
-it('should check if ends with question mark ?', function () {
+        //Assert :: verificar
+        $request->assertRedirect(route('dashboard'));
+        assertDatabaseCount('questions', 1);
+        assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?',
+        ]);
+    });
 
-    //Act :: agir
-    $request = post(route('question.store'), [
-        'question' => str_repeat('*', 10),
-    ]);
+    it('should check if ends with question mark ?', function () {
 
-    //Assert :: verificar
-    $request->assertSessionHasErrors(['question' => 'Are you sure that is a question? It is missing the question mark in the end.']);
-    assertDatabaseCount('questions', 0);
-});
+        //Act :: agir
+        $request = post(route('question.store'), [
+            'question' => str_repeat('*', 10),
+        ]);
 
-it('should have at least 10 characters', function () {
+        //Assert :: verificar
+        $request->assertSessionHasErrors(['question' => 'Are you sure that is a question? It is missing the question mark in the end.']);
+        assertDatabaseCount('questions', 0);
+    });
 
-    //Act :: agir
-    $request = post(route('question.store'), [
-        'question' => str_repeat('*', 7) . '?',
-    ]);
+    it('should have at least 10 characters', function () {
 
-    //Assert :: verificar
-    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
-    assertDatabaseCount('questions', 0);
-});
+        //Act :: agir
+        $request = post(route('question.store'), [
+            'question' => str_repeat('*', 7) . '?',
+        ]);
 
-it('should create as a draft all the time', function () {
+        //Assert :: verificar
+        $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+        assertDatabaseCount('questions', 0);
+    });
 
-    //Act :: agir
-    $request = post(route('question.store'), [
-        'question' => str_repeat('*', 260) . '?',
-    ]);
+    it('should create as a draft all the time', function () {
 
-    assertDatabaseHas('questions', [
-        'question' => str_repeat('*', 260) . '?',
-        'draft'    => true,
-    ]);
+        //Act :: agir
+        $request = post(route('question.store'), [
+            'question' => str_repeat('*', 260) . '?',
+        ]);
+
+        assertDatabaseHas('questions', [
+            'question' => str_repeat('*', 260) . '?',
+            'draft'    => true,
+        ]);
+    });
 });
 
 test('only authenticated users can create a new question', function () {
-
-    auth()->logout();
-
     post(route('question.store'), [
         'question' => str_repeat('*', 260) . '?',
     ])->assertRedirect(route('login'));
