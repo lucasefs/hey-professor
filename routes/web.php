58e99cd9a78af4cd46816022356e59d5bd05465d
Lucas Eduardo Fagundes <lucasfagundes0@outlook.com>
2024-03-10 20:06:23 -0300

    hey-20: it should be able to archive a question using soft delete

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::get('/', [QuestionController::class, 'index'])->name('index');
         Route::get('/{question}/edit', [QuestionController::class, 'edit'])->name('edit');
         Route::put('/{question}', [QuestionController::class, 'update'])->name('update');
+        Route::patch('/{question}/archive', [QuestionController::class, 'archive'])->name('archive');
         Route::delete('/{question}', [QuestionController::class, 'destroy'])->name('destroy');
         Route::post('/like/{question}', Question\LikeController::class)->name('like');
         Route::post('/unlike/{question}', Question\UnlikeController::class)->name('unlike');
